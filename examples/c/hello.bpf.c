4d8aad0b2962e728662521a1a512b9b46fe87886
Alan Cheng <chenghongbo@outlook.com>
2023-12-23 14:15:34 +0800

    add non-working lru trace version

--- examples/c/hello.bpf.c
+++ examples/c/hello.bpf.c
@@ -1,16 +1,18 @@
 #include "vmlinux.h"
-#include "bpf/bpf_helpers.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
 
 SEC("kprobe/add_to_page_cache_lru")
-int bpf_prog(void *ctx)
-{
-	char msg[] = "hello world!\n";
-//	bpf_trace_printk(msg, sizeof(msg));
-	char comm[16] = "";
+int trace_page_cache_lru_addition(struct pt_regs *ctx) {
 	struct task_struct *task;
+	struct page *page = (struct page *)PT_REGS_PARM1(ctx);
+	struct address_space *mapping = (struct address_space *)PT_REGS_PARM2(ctx);
+	struct inode *inode;
+	bpf_probe_read(&inode,sizeof(inode),&mapping->host);
+	u64 inumber = inode->i_ino;
 	task = (struct task_struct *)bpf_get_current_task();
-	bpf_get_current_comm(&comm,sizeof(comm));
-	bpf_trace_printk(comm,sizeof(comm));
+//bpf_get_current_comm(&comm,sizeof(comm));
+	bpf_printk("inode number: %llu\n",inumber);
 
 
 	return 0;
