b7c093a8b98d452c8094f095b059e561d576f4cd
Alan Cheng <chenghongbo@outlook.com>
2023-12-26 15:29:56 +0800

    working copy to get inode number from mapping argument

--- examples/c/hello.bpf.c
+++ examples/c/hello.bpf.c
@@ -1,18 +1,21 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+#define pgoff_t unsigned long
+
+//typedef unsigned int __bitwise gfp_t;
 
 SEC("kprobe/add_to_page_cache_lru")
-int trace_page_cache_lru_addition(struct pt_regs *ctx) {
-	struct task_struct *task;
-	struct page *page = (struct page *)PT_REGS_PARM1(ctx);
-	struct address_space *mapping = (struct address_space *)PT_REGS_PARM2(ctx);
-	struct inode *inode;
-	bpf_probe_read(&inode,sizeof(inode),&mapping->host);
-	u64 inumber = inode->i_ino;
-	task = (struct task_struct *)bpf_get_current_task();
-//bpf_get_current_comm(&comm,sizeof(comm));
+//int trace_page_cache_lru_addition(struct pt_regs *ctx) {
+int BPF_KPROBE(add_to_page_cache_lru,struct page *page, struct address_space *mapping) { //,pgoff_t offset, gfp_t gfp_mask) {
+	//struct page *page = (struct page *)PT_REGS_PARM1(ctx);
+	//struct address_space *mapping = (struct address_space *)PT_REGS_PARM2(ctx);
+        u64 inumber = BPF_CORE_READ(mapping,host,i_ino);
+	//bpf_probe_read(&inode,sizeof(inode),&mapping->host);
 	bpf_printk("inode number: %llu\n",inumber);
+	//task = (struct task_struct *)bpf_get_current_task();
+//bpf_get_current_comm(&comm,sizeof(comm));
 
 
 	return 0;
