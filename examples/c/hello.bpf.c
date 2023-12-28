7139b0b6b2260178b54b99e002165acf57a65984
Alan Cheng <chenghongbo@outlook.com>
2023-12-28 10:43:24 +0800

    non-working copy with maps to trace file open calls

--- examples/c/hello.bpf.c
+++ examples/c/hello.bpf.c
@@ -3,17 +3,85 @@
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
 #define pgoff_t unsigned long
-
+#define MAX_ENTRIES	8192
 //typedef unsigned int __bitwise gfp_t;
 
+struct data_t {
+    __u32 pid;
+    __u64 inode;
+    char filename[256];
+};
+
+/*
+BPF_HASH(tmp, u32, struct data_t);
+BPF_HASH(openfiles, u64, struct data_t);
+*/
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_ENTRIES);
+	__type(key, __u32);
+	__type(value, struct data_t);
+} tmp SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_ENTRIES);
+	__type(key, __u64);
+	__type(value, struct data_t);
+} openfiles SEC(".maps");
+
+SEC("kprobe/do_sys_open")
+int kprobe__do_sys_open(struct pt_regs *ctx)
+{
+    struct data_t data = {};
+    u32 pid = bpf_get_current_pid_tgid();
+
+    data.pid = pid;
+    bpf_probe_read(&data.filename, sizeof(data.filename), (void*)PT_REGS_PARM1(ctx));
+
+    bpf_map_update_elem(&tmp, &pid, &data, BPF_ANY);
+
+    return 0;
+}
+
+SEC("kprobe/vfs_open")
+int kprobe__vfs_open(struct pt_regs *ctx)
+{
+    struct file *file = (struct file *)PT_REGS_PARM1(ctx);
+    struct inode *inode = file->f_inode;
+
+    u32 pid = bpf_get_current_pid_tgid();
+    struct data_t *data = bpf_map_lookup_elem(&tmp,&pid);
+
+    if (!data) {
+        return 0; // missed entry
+    }
+
+    data->inode = inode->i_ino; // get the inode number
+    //openfiles.update(&data->inode, data);
+    bpf_map_update_elem(&openfiles, &pid, &data, BPF_ANY);
+
+    return 0;
+}
+
+SEC("kretprobe/do_sys_open")
+int kretprobe__do_sys_open(struct pt_regs *ctx)
+{
+    u32 pid = bpf_get_current_pid_tgid();
+
+    bpf_map_delete_elem(&tmp,&pid);
+    return 0;
+}
+
+
+/* trace page cache addition */
 SEC("kprobe/add_to_page_cache_lru")
 //int trace_page_cache_lru_addition(struct pt_regs *ctx) {
 int BPF_KPROBE(add_to_page_cache_lru,struct page *page, struct address_space *mapping) { //,pgoff_t offset, gfp_t gfp_mask) {
-	//struct page *page = (struct page *)PT_REGS_PARM1(ctx);
-	//struct address_space *mapping = (struct address_space *)PT_REGS_PARM2(ctx);
         u64 inumber = BPF_CORE_READ(mapping,host,i_ino);
-	//bpf_probe_read(&inode,sizeof(inode),&mapping->host);
 	bpf_printk("inode number: %llu\n",inumber);
+	//struct task_struct *task;
 	//task = (struct task_struct *)bpf_get_current_task();
 //bpf_get_current_comm(&comm,sizeof(comm));
 
